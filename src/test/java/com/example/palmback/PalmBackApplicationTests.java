02f651a491251311c4bf1d1b67d2adf101d1093d
escomputer <nujest307@naver.com>
2026-02-06 22:10:17 -0500

    git init ej

---
+++ src/test/java/com/example/palmback/PalmBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.palmback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalmBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
